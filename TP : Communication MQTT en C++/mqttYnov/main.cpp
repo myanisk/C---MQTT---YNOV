6b9f5e8bdb5c61d0c136478fd148874770f98476
myanisk <72413657+myanisk@users.noreply.github.com>
2023-04-14 15:37:43 +0200

    PAQL

--- TP : Communication MQTT en C++/mqttYnov/main.cpp
+++ TP : Communication MQTT en C++/mqttYnov/main.cpp
@@ -2,44 +2,44 @@
 #include <QtMqtt/QMqttClient> // Including the QMqttClient library to use an MQTT client
 #include <QDebug> // Including the qDebug library to display debug messages
 
-int main(int argc, char *argv[])
+int main(int argc_var, char *argv_t[])
 {
-    QCoreApplication a(argc, argv); // Creating an instance of Qt Core application
+    QCoreApplication A(argc_var, argv_t); // Creating an instance of Qt Core application
 
-    QMqttClient client; // Creating an MQTT client
+    QMqttClient Client; // Creating an MQTT client
 
     // Configuring MQTT client connection parameters
-    client.setHostname("broker.emqx.io"); // Setting the MQTT broker host
-    client.setPort(1883); // Setting the MQTT broker port
-    client.setUsername("yanis"); // Setting the MQTT broker username
-    client.setPassword("testynov"); // Setting the MQTT broker password
+    Client.setHostname("broker.emqx.io"); // Setting the MQTT broker host
+    Client.setPort(1883); // Setting the MQTT broker port
+    Client.setUsername("yanis"); // Setting the MQTT broker username
+    Client.setPassword("testynov"); // Setting the MQTT broker password
 
-    QObject::connect(&client, &QMqttClient::connected, [&](void) {
+    QObject::connect(&Client, &QMqttClient::connected, [&](void) {
         qDebug() << "Connected to MQTT broker.";
 
-        QString topic = "/ynov/bordeaux/";
-        qint32 qos = 0;
-        if (!client.subscribe(topic, qos)) { // Subscribing to an MQTT topic
-            qDebug() << "Error subscribing to topic:" << topic;
+        QString s_topic = "/ynov/bordeaux/";
+        qint32 qos_var = 0;
+        if (!Client.subscribe(s_topic, qos_var)) { // Subscribing to an MQTT topic
+            qDebug() << "Error subscribing to topic:" << s_topic;
             return 1;
         } else {
-            qDebug() << "Subscribed to topic:" << topic;
+            qDebug() << "Subscribed to topic:" << s_topic;
         }
 
-        QString message = "AppYanis";
-        if (!client.publish(topic, message.toUtf8(), qos)) { // Publishing an MQTT message
-            qDebug() << "Published message:" << message << "to topic:" << topic;
+        QString s_message = "AppYanis";
+        if (!Client.publish(s_topic, s_message.toUtf8(), qos_var)) { // Publishing an MQTT message
+            qDebug() << "Published message:" << s_message << "to topic:" << s_topic;
             return 1;
         } else {
-            qDebug() << "Error publishing message:" << message << "to topic:" << topic;
+            qDebug() << "Error publishing message:" << s_message << "to topic:" << s_topic;
         }
     });
 
-    QObject::connect(&client, &QMqttClient::messageReceived, [&](const QByteArray &message, const QMqttTopicName &topic) {
+    QObject::connect(&Client, &QMqttClient::messageReceived, [&](const QByteArray &message, const QMqttTopicName &topic) {
         qDebug() << "Received message:" << message << "from topic:" << topic.name();
     });
 
-    client.connectToHost(); // Connecting to the MQTT broker
+    Client.connectToHost(); // Connecting to the MQTT broker
 
-    return a.exec(); // Starting the Qt Core event loop
+    return A.exec(); // Starting the Qt Core event loop
 }
